5b502a91170109c79ff017066bcfddb9f9e1db37
tashi tsering <tatsering54@gmail.com>
2016-12-01 17:47:13 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,12 @@
+class Pattern
+{
+	public static void main (String[] args)
+	{
+	 System.out.println("*__________*\n");
+         System.out.println("__*_____*\n");
+	System.out.println("_____*\n");
+	System.out.println("____*____*\n");
+	System.out.println("*__________*");
+	
+	}
+}
